dfbee8c0aaaf05fb593e1c19454f001a04fad163
Robin Engels <103217628+robin-engels@users.noreply.github.com>
2025-10-22 15:06:16 +0200

    Create mocha-sh.rb

---
+++ Formula/mocha-sh.rb
@@ -0,0 +1,19 @@
+class MochaSh < Formula
+  include Language::Python::Virtualenv
+
+  desc "Mocha.sh — your cozy terminal companion ☕"
+  homepage "https://github.com/mocha-sh/mocha"
+  url "https://files.pythonhosted.org/packages/source/m/mocha-sh/mocha-sh-0.1.7.tar.gz"
+  sha256 "REPLACE_WITH_REAL_SHA256"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "Mocha.sh", shell_output("#{bin}/mocha --help")
+  end
+end
